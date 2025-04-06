86cab2a8501a900a37196ca3bad9657943b2f7fe
Aradhya Sharma <140966869+Sharmaaradhyaji@users.noreply.github.com>
2025-04-06 18:24:43 +0530

    final commit

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { auth, db } from "@/firebase/admin";
+import { redirect } from "next/dist/server/api-utils";
 import { cookies } from "next/headers";
 
 // Session duration (1 week)
